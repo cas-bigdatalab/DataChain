a00432df480ad525c634a180d4f553422dff2792
Duyyuan <duyyuan2014@gmail.com>
2016-06-12 18:47:58 +0800

    Delete old file

--- src/main/scala/com/cnic/datachain/compute/TestCompute.scala
+++
@@ -1,8 +0,0 @@
-package com.cnic.datachain.compute
-
-/**
- * Created by cnic on 2016/5/25.
- */
-object TestCompute {
-
-}
--- src/main/scala/com/cnic/datachain/store/TestStore.scala
+++
@@ -1,8 +0,0 @@
-package com.cnic.datachain.store
-
-/**
- * Created by cnic on 2016/5/25.
- */
-object TestStore {
-
-}
