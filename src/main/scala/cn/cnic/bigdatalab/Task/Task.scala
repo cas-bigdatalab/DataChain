910d525bea47b20470136e26d9b795d93d44ef30
Duyyuan <duyyuan2014@gmail.com>
2016-06-27 18:24:38 +0800

    Add cancel task function

--- src/main/scala/cn/cnic/bigdatalab/Task/Task.scala
+++ src/main/scala/cn/cnic/bigdatalab/Task/Task.scala
@@ -9,29 +9,82 @@ import com.github.casbigdatalab.datachain.transformer.Transformer
   */
 abstract class BaseTask() {
   protected var scheduler: Scheduler = _
+
   def run
 
+  def cancel(name: String)
+
 }
 
-class RealTimeTask(taskInstance: TaskInstance ) extends BaseTask(){
-  override def run(): Unit ={
-    this.scheduler = new RealTimeScheduler
+class RealTimeTask(taskInstance: TaskInstance) extends BaseTask() {
+  this.scheduler = new RealTimeScheduler
+
+  override def run(): Unit = {
     scheduler.deploy(taskInstance)
 
   }
-}
-class OfflineTask(taskInstance: TaskInstance) extends BaseTask(){
-  override def run(): Unit ={
-    this.scheduler = new OfflineScheduler
-    scheduler.deploy(taskInstance)
-  }
+
+  override def cancel(name: String): Unit = ???
 }
 
-class StoreTask(taskInstance: TaskInstance) extends BaseTask(){
-  override def run(): Unit ={
+class OfflineTask(taskInstance: TaskInstance) extends BaseTask() {
+  this.scheduler = new OfflineScheduler
+
+  override def run(): Unit = {
+    scheduler.deploy(taskInstance)
+  }
+
+  override def cancel(name: String): Unit ={
+    scheduler.cancel(name)
+  }
+
+}
+
+class StoreTask(taskInstance: TaskInstance) extends BaseTask() {
+  override def run(): Unit = {
     //create task
     //scheduler.deploy
 
+
+  }
+
+  override def cancel(name: String): Unit = ???
+}
+
+object TaskTest {
+  def main(args: Array[String]): Unit = {
+    val topic = "user"
+    val schema = new Schema()
+    schema.setDriver("mongo")
+    schema.setDb("test")
+    schema.setTable("user")
+    schema.setColumns(Map("id" -> "Int", "name" -> "String", "age" -> "String"))
+
+    val sql = "select * from user"
+
+    val task: TaskInstance = new TaskInstance().init("test_task", "realtime", sql, topic, schema, "mapping")
+
+    val realTimeTask = new RealTimeTask(task)
+    realTimeTask.run()
+
+    val topic1 = "user1"
+    val schema1 = new Schema()
+    schema1.setDriver("mongo")
+    schema1.setDb("test1")
+    schema1.setTable("user1")
+    schema1.setColumns(Map("id" -> "Int", "name" -> "String", "age" -> "String"))
+
+    val sql1 = "select * from user"
+
+    val task1: TaskInstance = new TaskInstance().init("test_task1", "offline", sql, topic, schema, "mapping")
+    task1.setInterval(5)
+
+    val offlineTask = new OfflineTask(task1)
+    offlineTask.run()
+
+    Thread.sleep(20*1000)
+
+    offlineTask.cancel(task1.getTaskType()+"_"+task1.getName())
 
   }
 
