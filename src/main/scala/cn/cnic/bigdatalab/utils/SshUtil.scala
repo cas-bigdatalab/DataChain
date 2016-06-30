7b5dcf0ee57334b550a52fa600752f5f1c1100c2
Duyyuan <duyyuan2014@gmail.com>
2016-06-30 09:39:44 +0800

    Test SSH OK

--- src/main/scala/cn/cnic/bigdatalab/utils/SshUtil.scala
+++ src/main/scala/cn/cnic/bigdatalab/utils/SshUtil.scala
@@ -20,7 +20,7 @@ object SshUtil {
   def login(ip: String, username: String, password: String): Boolean ={
     conn = new Connection(ip)
     conn.connect()
-    return conn.authenticateWithPassword(username, password)
+    conn.authenticateWithPassword(username, password)
   }
 
   def execWithParams(scriptsLocation: String, ip: String, username: String, password: String, param: String*): Unit = {
@@ -30,19 +30,19 @@ object SshUtil {
         scriptsParams += " "+p
       }
     }
-    return exec(scriptsLocation, ip, username, password)
+    exec(scriptsParams, ip, username, password)
   }
 
   def exec(scriptsLocation: String, ip: String, username: String , password: String): Int ={
     var stdOut: InputStream = null
     var stdErr: InputStream = null
-    var outStr: String = _
-    var outErr: String = _
+    var outStr: String = null
+    var outErr: String = null
     var ret = -1
     try {
       if (login(ip, username, password)) {
         val session: Session = conn.openSession()
-        session.execCommand("sh " + scriptsLocation)
+        session.execCommand(scriptsLocation)
 
         stdOut = new StreamGobbler(session.getStdout())
         outStr = processStream(stdOut, charset)
@@ -52,8 +52,8 @@ object SshUtil {
 
         session.waitForCondition(ChannelCondition.EXIT_STATUS, TIME_OUT)
 
-        System.out.println("outStr=" + outStr)
-        System.out.println("outErr=" + outErr)
+        println("outStr=" + outStr)
+        println("outErr=" + outErr)
 
         ret = session.getExitStatus()
       } else {
@@ -70,17 +70,17 @@ object SshUtil {
   }
 
   private def processStream(in: InputStream, charset: String): String = {
-    val buf = Array[Byte]()
+    val buf = new Array[Byte](1024)
     val sb = new StringBuilder()
     while (in.read(buf) != -1) {
       sb.append(new String(buf, charset))
     }
-    return sb.toString()
+    sb.toString()
   }
 
 
   def main(args: Array[String]): Unit ={
-    println("executeResult=" + SshUtil.execWithParams("ls -h ", "10.0.96.175", "root", "cnic.cn", "/opt"));
+    println("executeResult=" + SshUtil.execWithParams("ls -h ", "10.0.50.216", "root", "bigdata", "/opt/dyy"))
   }
 
 }
