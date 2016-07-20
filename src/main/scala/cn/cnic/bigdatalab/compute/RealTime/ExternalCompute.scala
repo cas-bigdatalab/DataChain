0be5765dae472e01c2241398ca91c4e54f4aa82f
Duyyuan <duyyuan2014@gmail.com>
2016-07-20 18:22:04 +0800

    Add annotation

--- src/main/scala/cn/cnic/bigdatalab/compute/RealTime/ExternalCompute.scala
+++ src/main/scala/cn/cnic/bigdatalab/compute/RealTime/ExternalCompute.scala
@@ -29,12 +29,12 @@ object ExternalCompute {
     StreamingLogLevels.setStreamingLogLevels()
 
     val conf = new SparkConf().setAppName(appName)
-      .setMaster("spark://10.0.71.1:7077")
-      .set("spark.driver.memory", "3g")
-      .set("spark.executor.memory", "10g")
-      .set("spark.cores.max", "12")
-      .set("spark.driver.allowMultipleContexts", "true")
-      .setJars(List("D:\\git\\DataChain\\out\\artifacts\\datachain_jar\\datachain.jar"))
+//      .setMaster("spark://10.0.71.1:7077")
+//      .set("spark.driver.memory", "3g")
+//      .set("spark.executor.memory", "10g")
+//      .set("spark.cores.max", "12")
+//      .set("spark.driver.allowMultipleContexts", "true")
+//      .setJars(List("D:\\git\\DataChain\\out\\artifacts\\datachain_jar\\datachain.jar"))
 
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(duration.toInt))
@@ -67,23 +67,23 @@ object ExternalCompute {
   }
 
   def main(args: Array[String]): Unit = {
-    val appName = "External test"
-    val duration = "1"
-    val topics = "test :1"
-    val kafkaParam = "zookeeper.connect->10.0.71.20:2181,10.0.71.26:2181,10.0.71.27:2181;group.id->test-consumer-group"
-    val mapping = "D:\\git\\DataChain\\conf\\csvMapping_user.json"
-    val externalPath = "D:\\git\\DataChain\\externalcode"
-    val filelist = "ConvertColumn.scala,ConnectMySql.scala"
-    val mainClass = "ConnectMySql"
+//    val appName = "External test"
+//    val duration = "1"
+//    val topics = "test :1"
+//    val kafkaParam = "zookeeper.connect->10.0.71.20:2181,10.0.71.26:2181,10.0.71.27:2181;group.id->test-consumer-group"
+//    val mapping = "D:\\git\\DataChain\\conf\\csvMapping_user.json"
+//    val externalPath = "D:\\git\\DataChain\\externalcode"
+//    val filelist = "ConvertColumn.scala,ConnectMySql.scala"
+//    val mainClass = "ConnectMySql"
 
-    //    val appName = args(0)
-    //    val duration = args(1)
-    //    val topics = args(2)
-    //    val kafkaParam = args(3)
-    //    val srcName = args(4)
-    //    val createDecTable = args(5)
-    //    val execSql = args(6)
-    //    val mapping = args(7)
+        val appName = args(0)
+        val duration = args(1)
+        val topics = args(2)
+        val kafkaParam = args(3)
+        val externalPath = args(4)
+        val filelist = args(5)
+        val mainClass = args(6)
+        val mapping = args(7)
 
     run(appName, duration, topics, kafkaParam, mainClass, externalPath, filelist, mapping)
   }
--- src/main/scala/cn/cnic/bigdatalab/compute/RealTime/SQLCompute.scala
+++ src/main/scala/cn/cnic/bigdatalab/compute/RealTime/SQLCompute.scala
@@ -30,12 +30,12 @@ object SQLCompute {
     StreamingLogLevels.setStreamingLogLevels()
 
     val conf = new SparkConf().setAppName(appName)
-      .setMaster("spark://10.0.71.1:7077")
-      .set("spark.driver.memory", "3g")
-      .set("spark.executor.memory", "10g")
-      .set("spark.cores.max", "12")
-      .set("spark.driver.allowMultipleContexts", "true")
-      .setJars(List("D:\\git\\DataChain\\out\\artifacts\\datachain_jar\\datachain.jar"))
+//      .setMaster("spark://10.0.71.1:7077")
+//      .set("spark.driver.memory", "3g")
+//      .set("spark.executor.memory", "10g")
+//      .set("spark.cores.max", "12")
+//      .set("spark.driver.allowMultipleContexts", "true")
+//      .setJars(List("D:\\git\\DataChain\\out\\artifacts\\datachain_jar\\datachain.jar"))
 
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(duration.toInt))
@@ -97,10 +97,10 @@ object SQLCompute {
 //                           |)""".stripMargin
 
     //hive test
-    val createDecTable = """
-                           |CREATE TABLE IF NOT EXISTS user(
-                           |name STRING, age INT
-                           |)""".stripMargin
+//    val createDecTable = """
+//                           |CREATE TABLE IF NOT EXISTS user(
+//                           |name STRING, age INT
+//                           |)""".stripMargin
 
 //    //Hbase test
 //    val createDecTable = """
@@ -118,26 +118,26 @@ object SQLCompute {
 //        |  collection     'user1',
 //        |  soft_commit_secs '5'
 //        |)""".stripMargin
-    val execSql = """
-                    |INSERT INTO table user
-                    |SELECT name, age FROM test
-                  """.stripMargin
-    val appName = "transformer test"
-    val duration = "1"
-    val topics = "test :1"
-    val kafkaParam = "zookeeper.connect->10.0.71.20:2181,10.0.71.26:2181,10.0.71.27:2181;group.id->test-consumer-group"
-    val schemaSrc = "id:Int,name :String,age:Int"
-    val srcName = "test"
-    val mapping = "D:\\git\\DataChain\\conf\\csvMapping_user.json"
+//    val execSql = """
+//                    |INSERT INTO table user
+//                    |SELECT name, age FROM test
+//                  """.stripMargin
+//    val appName = "transformer test"
+//    val duration = "1"
+//    val topics = "test :1"
+//    val kafkaParam = "zookeeper.connect->10.0.71.20:2181,10.0.71.26:2181,10.0.71.27:2181;group.id->test-consumer-group"
+//    val schemaSrc = "id:Int,name :String,age:Int"
+//    val srcName = "test"
+//    val mapping = "D:\\git\\DataChain\\conf\\csvMapping_user.json"
 
-//    val appName = args(0)
-//    val duration = args(1)
-//    val topics = args(2)
-//    val kafkaParam = args(3)
-//    val srcName = args(4)
-//    val createDecTable = args(5)
-//    val execSql = args(6)
-//    val mapping = args(7)
+    val appName = args(0)
+    val duration = args(1)
+    val topics = args(2)
+    val kafkaParam = args(3)
+    val srcName = args(4)
+    val createDecTable = args(5)
+    val execSql = args(6)
+    val mapping = args(7)
 
     println("create dec table sql:" + createDecTable)
     println("exec sql:" + execSql)
